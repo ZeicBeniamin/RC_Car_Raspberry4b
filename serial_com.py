8d94a8422ae86605813985ee4a2ea706e37b75c4
ZeicBeniamin <beniamin22052000@gmail.com>
2021-05-05 21:19:34 +0300

    Created UartHelper class, added logging functionality

--- serial_com.py
+++ serial_com.py
@@ -3,10 +3,11 @@ from time import sleep
 import serial
 import threading
 import time
+import datetime
+import os
 # Testing purposes only
 # TODO: Remove import after testing
 import argparse
-
 
 class SwapFlag(Enum):
     SWP_BUFFER_READY = 1
@@ -15,17 +16,6 @@ class SwapFlag(Enum):
 class RxFlag(Enum):
     RX_BUFFER_READY = 1 
     RX_BUFFER_BUSY_READING = 2
-
-# -------------------------- IMPORTANT TODO ---------------------------
-
-# TODO: Complete the TODO's in this file before proceding to anything
-#        else
-
-# -------------------------- IMPORTANT TODO - END ---------------------
-
-
-# TODO: Document the UartMessageHandler class better
-#       Add docstring to class and to each of the methods
 
 class UartMessageHandler:
     """ Stores messages received through UART; returns them on-demand
@@ -222,7 +212,7 @@ def parse(message):
 
     return rearranged
 
-def ser_read(ser, uart_msg_handler):
+def ser_read(ser, uart_msg_handler, log_file):
     """ Read 8 bytes at a time from the UART serial port in an infinite
         loop
 
@@ -234,16 +224,23 @@ def ser_read(ser, uart_msg_handler):
             Stores message received through UART. Enables message 
             transfers that are visible from the main thread.
     """
-    
-    while True:
-        try:
+    try:
+        while True:        
             reading = ser.read(8).decode()
             # Store raw message in uart handling class
             uart_msg_handler.receive_msg(reading)
-        except:
-            print("exception gen")
+            write_string = "Received: " + uart_msg_handler.read_msg() + "\n"
+            log_file.write(write_string)
+            log_file.flush()
+            os.fsync(log_file.fileno())
+            print("Received: " + uart_msg_handler.read_msg())
+    except:
+        print("Exception generated")
+    finally:
+        print ("Ended reading")
+        log_file.close()
 
-def ser_write(ser, uart_message_handler):
+def ser_write(ser, uart_message_handler, count, log_file):
     """ Write 8 bytes at a time to the UART serial port in an infinite
         loop
 
@@ -256,16 +253,39 @@ def ser_write(ser, uart_message_handler):
             bridge to other threads.
     """
     test = "pi.><ras"
-    test = "<CONACC>"
+    test = "<M15S80>"
+    
     while True:
-        # i += 1
-        try:
-            ser.write(test.encode())
+        try: 
+            count += 1
+            if count <= 10:
+                ser.write("<CONACC>".encode())
+                print("<CONNACC>")
+            else:
+                # Bug fix: count is now padded with zeros to form a number of at least two digits
+                test = f"<M00S{count%100:02}>"
+                ser.write(test.encode())
+                write_string = "send; sleep = 0.1; " + test + " " + str(count) + "\n"
+                log_file.write(write_string)
+                log_file.flush()
+                os.fsync(log_file.fileno())
+                print(write_string)
+            time.sleep(0.05)
         except:
-            print ("Write killed")
-        time.sleep(0.5)
-        print("send; sleep = 0.5")
+            print("Write - inner while error")
         
+
+def open_log_file():
+    global f
+    date = datetime.datetime.now();
+    date = date.strftime("%Y-%m-%d__%H:%M:%S")
+    print(date)
+
+    f = open("./logs/rpi_log_{}.txt".format(date), "w")
+
+f = None
+g = None
+g = open("./logs/Dummy_file", "w")
 
 # Open the serial port that connects to STM32. It may be connected on 
 # ACM0 or ACM1
@@ -276,19 +296,15 @@ except:
 
 uart_message_handler = UartMessageHandler()
 
+open_log_file()
+
 # Start two other threads for reading and writing to UART
-thread_read = threading.Thread(target=ser_read, args=(ser, uart_message_handler))
-thread_write = threading.Thread(target=ser_write, args=(ser, uart_message_handler))
+thread_read = threading.Thread(target=ser_read, args=(ser, uart_message_handler, f))
+thread_write = threading.Thread(target=ser_write, args=(ser, uart_message_handler, 0, f))
 
 thread_read.start()
 thread_write.start()
 
-
-# Print a message from main as an indication that the program is 
-# running
-while True:
-    
-    pass
 
 # TODO: Detail the print statements below - useful for debugging
 
@@ -296,6 +312,10 @@ print ("Working")
 print ("Sending and receiving messages at the following frequencies:")
 print ("TODO: Show frequency of tx and rx operations")
 
+while True:
+    pass
+
+f.close()
 
 # Close the serial port after using it
 ser.close()
---
+++ uart_messsage_handler.py
@@ -0,0 +1,29 @@
+class UartMessageHandler:
+    def __init__(self):
+        """ Initialize the variables of the class
+        """
+        self._rx_buffer1 = ""
+        self._rx_buffer2 = ""
+
+        # _in_use_buffer works as a "pointer" variable; it holds the
+        # addres of the currently used buffer 
+        self._in_use_buffer = self._rx_buffer1
+        pass
+
+    def receive_msg():
+        """ Receive UART message from reading thread 
+
+        UART message is stored in the attributes of the class. We use a
+        double buffer system for storing data
+
+        """
+
+        if (_swap_flag == BUFFER_READY):
+            if(_in_use_buffer)
+        pass
+
+    def read_msg():
+        
+        pass
+
+
